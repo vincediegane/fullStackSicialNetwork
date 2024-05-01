8369e41b74f96bd2706e0bed2116d46196ad4b86
babacark <babacar.k@itdm-group.com>
2024-05-01 17:51:27 +0100

    - adding get all profiles & get profile user profile in the whitelist url  - adding images in the front side

--- devconnector/src/main/java/com/devconnector/config/SecurityConfiguration.java
+++ devconnector/src/main/java/com/devconnector/config/SecurityConfiguration.java
@@ -27,6 +27,7 @@ public class SecurityConfiguration {
     private static final String[] WHITE_LIST_URL = {
         "/api/v1/auth/**",
         "/api/v1/profiles/",
+        "/api/v1/profiles/users/**",
         "/v2/api-docs",
         "/v3/api-docs",
         "/v3/api-docs/**",
