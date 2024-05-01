96a2823a96a5f757478a3b5ca36b6cf7cb0a4b7c
babacark <babacar.k@itdm-group.com>
2024-05-01 05:37:45 +0100

    refactoring login wip

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthApiService } from 'src/app/services/services';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  errorMessage: string = "";
+  userFormGroup!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthApiService) { }
 
   ngOnInit(): void {
+    this.userFormGroup = this.fb.group({
+      email: this.fb.control(""),
+      password: this.fb.control("")
+    });
+  }
+
+  login() {
+    let email = this.userFormGroup.value.email;
+    let password = this.userFormGroup.value.password;
+
+    this.authService.authenticate({
+      body: {email, password}
+    }).subscribe({
+      next: (res) => {
+        debugger
+        console.log(res);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  register() {
+    this.router.navigateByUrl("/register");
   }
 
 }
